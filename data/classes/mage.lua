5cce33b2bda64db65cc7972f3e5071f2bb0dff8c
coolmodi <7333074+coolmodi@users.noreply.github.com>
2019-10-23 09:56:50 +0200

    update mage to new spell data system

--- data/classes/mage.lua
+++ data/classes/mage.lua
@@ -18,176 +18,6 @@ local BLAST_WAVE = GetSpellInfo(11113);
 local MAGE_ARMOR = GetSpellInfo(6117);
 local BLIZZARD = GetSpellInfo(10);
 
-local ARCANE_EXPLOSION_GENERIC = {
-    school = _addon.SCHOOL.ARCANE,
-    ttMinMax = "(%d+).-%s(%d+)",
-    coef = 0.214
-};
-
-local FIRE_BLAST_GENERIC = {
-    school = _addon.SCHOOL.FIRE,
-    ttMinMax = "(%d+).-%s(%d+)",
-    coef = 1.5/3.5
-};
-
-local FROST_BOLT_GENERIC = {
-    school = _addon.SCHOOL.FROST,
-    ttMinMax = "(%d+).-%s(%d+)",
-    coef = 3 / 3.5 * 0.95
-};
-
-local FLAME_STRIKE_GENERIC = {
-    school = _addon.SCHOOL.FIRE,
-    primary = {
-        ttMinMax = "(%d+).-%s(%d+)",
-        coef = 0.264
-    },
-    secondary = {
-        isDuration = true,
-        duration = 8,
-        tickPeriod = 2,
-        ttMinMax = ".*%s(%d+).*%s%d",
-        coef = 0.025,
-    }
-};
-
-_addon.spellData = {
-    [1449] = _addon:SpellDataMerge(ARCANE_EXPLOSION_GENERIC, {level = 14}),
-    [ARCANE_EXPLOSION] = ARCANE_EXPLOSION_GENERIC,
-
-    [133] = {
-        school = _addon.SCHOOL.FIRE,
-        level = 1,
-        primary = {
-            ttMinMax = "(%d+).-%s(%d+)",
-            coef = 1.5/3.5
-        },
-        secondary = {
-            isDuration = true,
-            duration = 4,
-            tickPeriod = 2,
-            ttMinMax = ".*%s(%d+).*%s%d",
-            coef = 0,
-        }
-    },
-    [143] = {
-        school = _addon.SCHOOL.FIRE,
-        level = 6,
-        primary = {
-            ttMinMax = "(%d+).-%s(%d+)",
-            coef = 2/3.5
-        },
-        secondary = {
-            isDuration = true,
-            duration = 6,
-            tickPeriod = 2,
-            ttMinMax = ".*%s(%d+).*%s%d",
-            coef = 0,
-        }
-    },
-    [145] = {
-        school = _addon.SCHOOL.FIRE,
-        level = 12,
-        primary = {
-            ttMinMax = "(%d+).-%s(%d+)",
-            coef = 2.5/3.5
-        },
-        secondary = {
-            isDuration = true,
-            duration = 6,
-            tickPeriod = 2,
-            ttMinMax = ".*%s(%d+).*%s%d",
-            coef = 0,
-        }
-    },
-    [3140] = {
-        school = _addon.SCHOOL.FIRE,
-        level = 18,
-        primary = {
-            ttMinMax = "(%d+).-%s(%d+)",
-            coef = 3/3.5
-        },
-        secondary = {
-            isDuration = true,
-            duration = 8,
-            tickPeriod = 2,
-            ttMinMax = ".*%s(%d+).*%s%d",
-            coef = 0,
-        }
-    },
-    [FIREBALL] = {
-        school = _addon.SCHOOL.FIRE,
-        primary = {
-            ttMinMax = "(%d+).-%s(%d+)",
-            coef = 1
-        },
-        secondary = {
-            isDuration = true,
-            duration = 8,
-            tickPeriod = 2,
-            ttMinMax = ".*%s(%d+).*%s%d",
-            coef = 0,
-        }
-    },
-
-    [PYROBLAST] = {
-        school = _addon.SCHOOL.FIRE,
-        primary = {
-            ttMinMax = "(%d+).-%s(%d+)",
-            coef = 1
-        },
-        secondary = {
-            isDuration = true,
-            duration = 12,
-            tickPeriod = 3,
-            ttMinMax = ".*%s(%d+).*%s%d",
-            coef = 0.175,
-        }
-    },
-
-    [2136] = _addon:SpellDataMerge(FIRE_BLAST_GENERIC, {level = 6}),
-    [2137] = _addon:SpellDataMerge(FIRE_BLAST_GENERIC, {level = 14}),
-    [FIRE_BLAST] = FIRE_BLAST_GENERIC,
-
-    [SCORCH] = {
-        school = _addon.SCHOOL.FIRE,
-        ttMinMax = "(%d+).-%s(%d+)",
-        coef = 1.5/3.5
-    },
-
-    [2120] = _addon:SpellDataMerge(FLAME_STRIKE_GENERIC, {level = 16}),
-    [FLAME_STRIKE] = FLAME_STRIKE_GENERIC,
-
-    [116] = _addon:SpellDataMerge(FROST_BOLT_GENERIC, {level = 4, coef = 1.5 / 3.5 * 0.95}),
-    [205] = _addon:SpellDataMerge(FROST_BOLT_GENERIC, {level = 8, coef = 1.8 / 3.5 * 0.95}),
-    [837] = _addon:SpellDataMerge(FROST_BOLT_GENERIC, {level = 14, coef = 2.2 / 3.5 * 0.95}),
-    [7322] = _addon:SpellDataMerge(FROST_BOLT_GENERIC, {coef = 2.6 / 3.5 * 0.95}),
-    [FROST_BOLT] = FROST_BOLT_GENERIC,
-
-    [CONE_OF_COLD] = {
-        school = _addon.SCHOOL.FROST,
-        ttMinMax = "(%d+).-%s(%d+)",
-        coef = 0.136
-    },
-
-    [BLAST_WAVE] = {
-        school = _addon.SCHOOL.FIRE,
-        ttMinMax = "(%d+).-%s(%d+)",
-        coef = 0.136
-    },
-
-    [BLIZZARD] = {
-        school = _addon.SCHOOL.FROST,
-        isDuration = true,
-        isChannel = true,
-        isChannelAoe = true,
-        duration = 8,
-        tickPeriod = 1,
-        ttMinMax = "(%d+)",
-        coef = 1/7,
-    },
-};
-
 _addon.talentData = {
     { -- Arcane Subtlety
         tree = 1,
@@ -237,13 +67,13 @@ _addon.talentData = {
         talent = 15,
         effects = {
             {
-                type = _addon.EFFECT_TYPE.MOD_EFFECT,
-                affectSchool = _addon.SCHOOL_MASK.ALL,
+                type = _addon.EFFECT_TYPE.MOD_DMG_DONE,
+                affectSchool = _addon.SCHOOL_MASK.ALL_SPELL,
                 perPoint = 1
             },
             {
                 type = _addon.EFFECT_TYPE.MOD_CRIT,
-                affectSchool = _addon.SCHOOL_MASK.ALL,
+                affectSchool = _addon.SCHOOL_MASK.ALL_SPELL,
                 perPoint = 1
             }
         }
@@ -357,7 +187,7 @@ _addon.talentData = {
 
 _addon.buffData[12042] = { -- Arcane Power
     effect = _addon.EFFECT_TYPE.MOD_DMG_DONE,
-    affectSchool = _addon.SCHOOL_MASK.ALL,
+    affectSchool = _addon.SCHOOL_MASK.ALL_SPELL,
     value = 30,
 };
 
