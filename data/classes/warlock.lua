a57792435f6cd3bac0d6427fc530e9370b5caf40
coolmodi <7333074+coolmodi@users.noreply.github.com>
2021-04-13 16:53:22 +0200

    Warlock talents

--- data/classes/warlock.lua
+++ data/classes/warlock.lua
@@ -1,3 +1,4 @@
+---@type AddonEnv
 local _, _addon = ...;
 local _, playerClass = UnitClass("player");
 if playerClass ~= "WARLOCK" then
@@ -7,24 +8,16 @@ end
 local L = _addon:GetLocalization();
 
 _addon.talentData = {
+    -----------------------------
+    -- Affliction
+    -----------------------------
     { -- Supression
         tree = 1,
         talent = 1,
         effects = {
             {
                 type = _addon.EFFECT_TYPE.SPELLMOD_FLAT_HIT_CHANCE,
-                affectSpell = {-2142714854, 1},
-                perPoint = 2
-            }
-        }
-    },
-    { -- Improved Drain Life
-        tree = 1,
-        talent = 6,
-        effects = {
-            {
-                type = _addon.EFFECT_TYPE.SPELLMOD_PCT_EFFECT,
-                affectSpell = {8},
+                affectSpell = {-2142714854, 3867},
                 perPoint = 2
             }
         }
@@ -34,8 +27,19 @@ _addon.talentData = {
         talent = 7,
         effects = {
             {
-                type = _addon.EFFECT_TYPE.SPELLMOD_PCT_EFFECT,
+                type = _addon.EFFECT_TYPE.SPELLMOD_PCT_DAMAGE,
                 affectSpell = {1024},
+                perPoint = 5
+            }
+        }
+    },
+    { -- Empowered Corruption
+        tree = 1,
+        talent = 12,
+        effects = {
+            {
+                type = _addon.EFFECT_TYPE.SPELLMOD_FLAT_SPELL_SCALE,
+                affectSpell = {2},
                 perPoint = 2
             }
         }
@@ -46,17 +50,25 @@ _addon.talentData = {
         effects = {
             {
                 type = _addon.EFFECT_TYPE.SPELLMOD_PCT_DAMAGE,
-                affectSpell = {145 + 2},
-                perPoint = 2
-            },
-            {
-                type = _addon.EFFECT_TYPE.SPELLMOD_PCT_EFFECT,
-                affectSpell = {541704, 1},
+                affectSpell = {541851, 6417},
                 perPoint = 2
             }
         }
     },
-
+    { -- Contagion
+        tree = 1,
+        talent = 17,
+        effects = {
+            {
+                type = _addon.EFFECT_TYPE.SPELLMOD_PCT_DAMAGE,
+                affectSpell = {1026, 16},
+                perPoint = 1
+            }
+        }
+    },
+    -----------------------------
+    -- Demonology
+    -----------------------------
     { -- Improved Health Funnel
         tree = 2,
         talent = 4,
@@ -68,7 +80,9 @@ _addon.talentData = {
             }
         }
     },
-
+    -----------------------------
+    -- Destruction
+    -----------------------------
     { -- Improved Shadow Bolt
         tree = 3,
         talent = 1,
@@ -85,7 +99,7 @@ _addon.talentData = {
         effects = {
             {
                 type = _addon.EFFECT_TYPE.SPELLMOD_FLAT_CRIT_CHANCE,
-                affectSpell = {997},
+                affectSpell = {901, 4288},
                 perPoint = 1
             }
         }
@@ -97,7 +111,8 @@ _addon.talentData = {
             {
                 type = _addon.EFFECT_TYPE.SPELLMOD_FLAT_CRIT_CHANCE,
                 affectSpell = {256},
-                perPoint = 2
+                base = 1,
+                perPoint = 3
             }
         }
     },
@@ -118,25 +133,36 @@ _addon.talentData = {
         effects = {
             {
                 type = _addon.EFFECT_TYPE.SPELLMOD_PCT_CRIT_MULT,
-                affectSpell = {997},
+                affectSpell = {965, 4288},
                 perPoint = 100
             }
         }
     },
     { -- Emberstorm
         tree = 3,
-        talent = 15,
+        talent = 16,
         effects = {
             {
                 type = _addon.EFFECT_TYPE.SPELLMOD_PCT_DAMAGE,
-                affectSpell = {868},
+                affectSpell = {868, 192},
                 perPoint = 2
+            }
+        }
+    },
+    { -- Shadow and Flame
+        tree = 3,
+        talent = 20,
+        effects = {
+            {
+                type = _addon.EFFECT_TYPE.SPELLMOD_FLAT_SPELL_SCALE,
+                affectSpell = {1, 64},
+                perPoint = 4
             }
         }
     },
 }
 
--- Amplify Curse
+-- Amplify Curse TODO
 _addon.aurasPlayer[18288] = {
     type = _addon.EFFECT_TYPE.SPELLMOD_PCT_EFFECT,
     affectSpell = {1024},
@@ -157,7 +183,7 @@ _addon.aurasPlayer[18789] = {
     value = 15,
 };
 
--- Master Demonologist dmg done
+-- Master Demonologist Succubus
 _addon.aurasPlayer[23761] = {
     type = _addon.EFFECT_TYPE.SCHOOLMOD_PCT_DAMAGE,
     affectMask = _addon.SCHOOL_MASK.ALL,
@@ -182,6 +208,32 @@ _addon.aurasPlayer[23836] = {
     type = _addon.EFFECT_TYPE.SCHOOLMOD_PCT_DAMAGE,
     affectMask = _addon.SCHOOL_MASK.ALL,
     value = 10,
+};
+-- Master Demonologist Felguard
+_addon.aurasPlayer[35702] = {
+    type = _addon.EFFECT_TYPE.SCHOOLMOD_PCT_DAMAGE,
+    affectMask = _addon.SCHOOL_MASK.ALL,
+    value = 1,
+};
+_addon.aurasPlayer[35703] = {
+    type = _addon.EFFECT_TYPE.SCHOOLMOD_PCT_DAMAGE,
+    affectMask = _addon.SCHOOL_MASK.ALL,
+    value = 2,
+};
+_addon.aurasPlayer[35704] = {
+    type = _addon.EFFECT_TYPE.SCHOOLMOD_PCT_DAMAGE,
+    affectMask = _addon.SCHOOL_MASK.ALL,
+    value = 3,
+};
+_addon.aurasPlayer[35705] = {
+    type = _addon.EFFECT_TYPE.SCHOOLMOD_PCT_DAMAGE,
+    affectMask = _addon.SCHOOL_MASK.ALL,
+    value = 4,
+};
+_addon.aurasPlayer[35706] = {
+    type = _addon.EFFECT_TYPE.SCHOOLMOD_PCT_DAMAGE,
+    affectMask = _addon.SCHOOL_MASK.ALL,
+    value = 5,
 };
 
 -- Soul Link
