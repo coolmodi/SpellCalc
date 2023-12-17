f8c4782c58fd0d751b8e8135135a89aced06d421
coolmodi <7333074+coolmodi@users.noreply.github.com>
2023-12-17 15:24:42 +0100

    SoD: Water Shield (maybe)

--- data/classic/classes/shaman.lua
+++ data/classic/classes/shaman.lua
@@ -17,6 +17,14 @@ _addon.aurasPlayer[16188] = {
     }
 };
 
+-- Water Shield
+_addon.aurasPlayer[408510] = {
+    {
+        type = _addon.CONST.EFFECT_TYPE.SCRIPT_AURASCRIPT,
+        scriptKey = "Water_Shield",
+    }
+};
+
 -- TODO: Convert T3 effect to new system
 --[[ local T3LightningShieldEffect = {
     condition = _addon.CONST.BUFF_CONDITIONS.EARTHSHATTERER_8PCS,
@@ -31,3 +39,24 @@ _addon.aurasPlayer[8134] = T3LightningShieldEffect;
 _addon.aurasPlayer[10431] = T3LightningShieldEffect;
 _addon.aurasPlayer[10432] = T3LightningShieldEffect;
  ]]
+
+ --------------------------------------------------------------------------
+-- Scripts
+--------------------------------------------------------------------------
+
+---@type AuraEffectBase
+local wsAura = {
+    type = _addon.CONST.EFFECT_TYPE.MOD_MANA_PER_5,
+}
+_addon.scripting.RegisterAuraScript("Water_Shield", function (apply, auraId, fromPlayer, scriptType, cacheValue)
+    local name = "WaterShield";
+    if apply then
+        local mana = UnitPowerMax("player", 0);
+        local restorePer5 = (1 / 500) * mana;
+        _addon:ApplyAuraEffect(name, wsAura, restorePer5, auraId, fromPlayer);
+        return restorePer5;
+    else
+        assert(cacheValue, "Water Shield aura removed while cacheValue is nil!");
+        _addon:RemoveAuraEffect(name, wsAura, cacheValue, auraId, fromPlayer);
+    end
+end);
