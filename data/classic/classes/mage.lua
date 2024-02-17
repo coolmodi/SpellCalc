0ddf74e7669835bcfd75590fd33512c631ca75c6
coolmodi <7333074+coolmodi@users.noreply.github.com>
2024-02-18 00:47:37 +0100

    Mage Spell Power rune

--- data/classic/classes/mage.lua
+++ data/classic/classes/mage.lua
@@ -18,3 +18,10 @@ local MageArmorEffect = {
 _addon.aurasPlayer[6117] = MageArmorEffect;
 _addon.aurasPlayer[22782] = MageArmorEffect;
 _addon.aurasPlayer[22783] = MageArmorEffect;
+
+--------------------------------------------------------------------------
+-- Runes
+--------------------------------------------------------------------------
+
+-- Feet: Spell Power
+_addon:DefineRuneSpell(412322, _addon.aurasPlayer[412322]);
